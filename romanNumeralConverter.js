34033419699edc3b4b60f3f99275c62addb9211b
nootaholic <lpsjsn94@gmail.com>
2022-08-11 07:53:36 -0400

    Roman numeral converter but cleaner

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -1,68 +1,34 @@
-// Convert the given number into a roman numeral.
-
-
+// Convert the given number into a roman numeral (redone and cleaned up)
 
 
 function convertToRoman(num) {
+  let roman = [
+    {name: "M", val: 1000},
+    {name: "CM", val: 900},
+    {name: "D", val: 500},
+    {name: "CD", val: 400},
+    {name: "C", val: 100},
+    {name: "XC", val: 90},
+    {name: "L", val: 50},
+    {name: "XL", val: 40},
+    {name: "X", val: 10},
+    {name: "IX", val: 9},
+    {name: "V", val: 5},
+    {name: "IV", val: 4},
+    {name: "I", val: 1}
+  ]
+  let numeral = ""; 
 
-    let M = 1000;
-    let CM = 900;
-    let D = 500;
-    let CD = 400;
-    let C = 100;
-    let XC = 90;
-    let L = 50;
-    let XL = 40;
-    let X = 10;
-    let IX = 9;
-    let V = 5;
-    let IV = 4;
-    let I = 1;
-    
-    let romanNumeral = "";
-    
-    while (num > 0) {
-             if (num >= M){
-          romanNumeral += "M"
-          num -= M;  } 
-        else if (num >= CM) {
-          romanNumeral += "CM"
-          num -= CM; }
-        else if (num >= D) {
-          romanNumeral += "D"
-          num -= D; }
-        else if (num >= CD) {
-          romanNumeral += "CD"
-          num -= CD; }
-        else if (num >= C) {
-          romanNumeral += "C"
-          num -= C; }
-        else if (num >= XC) {
-          romanNumeral += "XC"
-          num -= XC; }
-        else if (num >= L) {
-          romanNumeral += "L"
-          num -= L; }
-        else if (num >= XL) {
-          romanNumeral += "XL"
-          num -= XL; }
-        else if (num >= X) {
-          romanNumeral += "X"
-          num -= X; }
-        else if (num >= IX) {
-          romanNumeral += "IX"
-          num -= IX; }
-        else if (num >= V) {
-          romanNumeral += "V"
-          num -= V; }
-        else if (num >= IV) {
-          romanNumeral += "IV"
-          num -= IV; }
-        else {
-          romanNumeral += "I"
-          num -= I; }
-    }
-    return romanNumeral
-    }
-    
-    convertToRoman(3)
+  numeral = roman.map(nome => {
+    let str = ""
+    while (nome.val <= num) { 
+      num -= nome.val;
+      str += nome.name
+    } return str
+  })
+  .join('')
+return numeral
+}
+  
+  convertToRoman(13)
+  
