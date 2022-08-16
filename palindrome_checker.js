ed1d9a2579d5f39459b56368fafeb2ad1277ccef
nootaholic <lpsjsn94@gmail.com>
2022-08-15 22:16:24 -0400

    updated palindrome checker with clean solution

--- palindrome_checker.js
+++ palindrome_checker.js
@@ -1,21 +1,17 @@
-// Return true if the given string is a palindrome. Otherwise, return false.
+function palindrome(str) { // updated for readability
+  let cleanString = str.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
+  let palLength = Math.round(cleanString.length);
+  let startIndex = 0;
+  let midIndex = palLength / 2;
+  let endIndex = palLength - 1;
 
-
-
-function palindrome(str) {
-    // check if string first index and last index are the same
-    // ignore non letters and spaces
-    // count comparisons until the middle // round down str.length / 2 
-    let cleanString = str.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
-    let palLength = Math.round(cleanString.length);
-    let midIndex = palLength / 2;
-    
-    for (let startDex = 0, endDex = palLength - 1 ; startDex <= midIndex ; startDex++, endDex--)
-      if (cleanString[startDex] !== cleanString[endDex]){
-      return false; }
-    
-      return true;
+  while (startIndex <= midIndex) {
+    startIndex++;
+    endIndex--;
+    if (cleanString[startIndex] !== cleanString[endIndex]){
+      return false; 
     }
-    
-    palindrome("eye");
+  } return true;
+}
 
+  palindrome("eye");
